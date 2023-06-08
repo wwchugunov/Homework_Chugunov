b1a5383ffcc0c1e1a30ab917f1eb8d1d0a05a28a
wwchugunov <78274510+wwchugunov@users.noreply.github.com>
2023-06-08 10:19:39 +0300

    fix function

--- Homework_1/main.js
+++ Homework_1/main.js
@@ -21,17 +21,19 @@ let randomDiscount = Math.random() * (sneakers,  notebook) + sneakers
 console.log(randomDiscount.toFixed(2))
 
 
-function randomDiscountf () {
-    let summ = document.getElementById('prise').value
-    console.log(`Сума: ${summ}`)
-    let  discont = document.getElementById('discont').value / 100;
-    console.log(`Знижка: ${discont * 100}%`)
-    let result = summ / 2
-    console.log(`Делим на 2: ${result}`)
-    let netPoint = result - (result * discont)
-    console.log(`Прибуток: ${netPoint}`)
-    alert(`Прибуток: ${netPoint}`)
+function randomDiscountf() {
+    let price = parseFloat(document.getElementById('prise').value);
+    let cost = price * 0.5;
+    let discount = parseFloat(document.getElementById('discont').value);
+    let discountedPrice = price - (price * (discount / 100));
+    let profit = discountedPrice - cost;
+    alert(`Чистий прибуток: ${profit} грн`);
 }
+  
+
+
+
+
 
 let min, max
 function minmax(sneakers, watch, notebook) {
@@ -53,9 +55,29 @@ let boxContent = `
 Складіть вартість всіх товарів, помістіть її в змінну та виведіть цю суму:   <span style="color: red;">${WholeAmount}</span><br>&emsp;
 Відкиньте копійки у всіх товарів, потім – складіть цілу частину вартості кожного товару між собою.:   <span style="color: red;">${noKopecks}</span><br>&emsp;
 Виведіть булеве значення: чи є сума всіх товарів (округлена в меншу сторону) парним чи непарним числом?:  <span style="color: red;">${isOdd}</span><br>&emsp;
-Виведіть суму решти, при оплаті всіх товарів (без округлення), якщо клієнт платить 500 грн.:  <span style="color: red;">${isOdd}</span><br>&emsp;
-Виведіть булеве значення: чи є сума всіх товарів (округлена в меншу сторону) парним чи непарним числом?:  <span style="color: red;">${summall}</span><br>&emsp;
+Виведіть суму решти, при оплаті всіх товарів (без округлення), якщо клієнт платить 500 грн.:  <span style="color: red;">${summall}</span><br>&emsp;
 Зробіть клієнту випадкову знижку та виведіть суму до оплати округлену до 2 знаків після коми.:  <span style="color: red;">${randomDiscount.toFixed(2)}</span><br>&emsp;
 `;
 
 infobox.innerHTML = boxContent;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
