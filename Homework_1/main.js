aa4094fd0a7d40707079074d698781ea66e20202
wwchugunov <78274510+wwchugunov@users.noreply.github.com>
2023-06-08 12:47:41 +0300

    Update main.js
    all

--- Homework_1/main.js
+++ Homework_1/main.js
@@ -9,6 +9,8 @@ let minnum = Math.floor(sneakers)
 console.log(minnum)
 let WholeAmount = (sneakers + watch + notebook)
 console.log(WholeAmount)
+let summallround = Math.round(WholeAmount);
+console.log(summallround)
 let  noKopecks = Math.floor(sneakers + watch + notebook)
 console.log(noKopecks)
 let rounding = (Math.ceil(noKopecks / 100) * 100)
@@ -19,6 +21,7 @@ let summall = (sneakers + watch + notebook - 500);
 console.log(summall)
 let randomDiscount = Math.random() * (sneakers,  notebook) + sneakers 
 console.log(randomDiscount.toFixed(2))
+let average = ((sneakers + watch + notebook  / 3).toFixed(2));
 
 function randomDiscountf() {
     let price = parseFloat(document.getElementById('prise').value);
@@ -32,8 +35,8 @@ function randomDiscountf() {
 function minmax(sneakers, watch, notebook) {
      min = Math.min(sneakers, watch, notebook);
      max = Math.max(sneakers, watch, notebook);
-    console.log(`Мінімальна ціна:${min} \nМаксимальна ціна ${max}`);
-    return  [min, max];
+     console.log(`Мінімальна ціна:${min} \nМаксимальна ціна ${max}`);
+     return  [min, max];
 }
 
 minmax(sneakers, watch, notebook)
@@ -45,9 +48,11 @@ let boxContent = `
 Використовуючи вбудований об'єкт Math – виведіть максимальне число:  <span style="color: red;"> ${maxnum}</span><br>&emsp; 
 Використовуючи вбудований об'єкт Math – виведіть мінімальне число:   <span style="color: red;">${minnum}</span><br>&emsp;
 Складіть вартість всіх товарів, помістіть її в змінну та виведіть цю суму:   <span style="color: red;">${WholeAmount}</span><br>&emsp;
-Відкиньте копійки у всіх товарів, потім – складіть цілу частину вартості кожного товару між собою.:   <span style="color: red;">${noKopecks}</span><br>&emsp;
+Відкиньте копійки у всіх товарів, потім – складіть цілу частину вартості кожного товару між собою:   <span style="color: red;">${noKopecks}</span><br>&emsp;
+Виведіть суму товарів округлену до сотень. (Наприклад якщо вийшло 260,то виведіть 300):   <span style="color: red;">${rounding}</span><br>&emsp;
 Виведіть булеве значення: чи є сума всіх товарів (округлена в меншу сторону) парним чи непарним числом?:  <span style="color: red;">${isOdd}</span><br>&emsp;
-Виведіть суму решти, при оплаті всіх товарів (без округлення), якщо клієнт платить 500 грн.:  <span style="color: red;">${summall}</span><br>&emsp;
+Виведіть суму решти, при оплаті всіх товарів (без округлення), якщо клієнт платить 500 грн:  <span style="color: red;">${summall}</span><br>&emsp;
+Виведіть середнє значення цін, округлене до другого знаку після коми:  <span style="color: red;">${average}</span><br>&emsp;
 Зробіть клієнту випадкову знижку та виведіть суму до оплати округлену до 2 знаків після коми.:  <span style="color: red;">${randomDiscount.toFixed(2)}</span><br>&emsp;
 `;
 
