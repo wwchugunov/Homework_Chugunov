b7f243d9ca345c1cec7e13b8a6bc6c06177e6f4b
wwchugunov <w.w.chugunov@gmail.com>
2023-06-10 21:04:04 +0300

    commit fixcion

--- Homework_1/main.js
+++ Homework_1/main.js
@@ -1,43 +1,7 @@
-let sneakers, watch, notebook, nocoins
-
-sneakers = 15.678;
-watch = 123.965;
-notebook = 90.2345;
-
-let summ = sneakers + watch + notebook
-
-function minmax(sneakers, watch, notebook) {
-    let min = Math.min(sneakers, watch, notebook);
-    let max = Math.max(sneakers, watch, notebook);
-    console.log(`Мінімальна ціна:${min} \nМаксимальна ціна ${max} \nВартість всіхтоварів: ${summ}`);
-    return  [min, max];
-}
 
 
-console.log(`Виведіть суму решти, при оплаті всіх товарів (без округлення), якщо клієнт платить 500 грн. \nРішення:  ${summ - 500}`)
-
-console.log(`Використовуючи вбудований об'єкт Math – виведіть максимальне число \nРішення:  ${Math.ceil(sneakers)} `)
-
-console.log(`Використовуючи вбудований об'єкт Math – виведіть мінімальне число \nРішення:  ${Math.floor(sneakers)}`) 
-
-console.log(`Складіть вартість всіх товарів, помістіть її в змінну та виведіть цю суму \nРішення:  ${summ}`) 
-
-sneakers = Math.floor(sneakers);
-watch = Math.floor(watch);
-notebook = Math.floor(notebook);
-
-nocoins = sneakers + watch + notebook
-console.log(`Відкиньте копійки у всіх товарів, потім – складіть цілу частину вартості кожного товару між собою.\nРішення:  ${nocoins}`)
-
-nocoins = Math.ceil(nocoins / 100) * 100;
-console.log(`Виведіть суму товарів округлену до сотень. (Наприклад якщо вийшло 260, то виведіть 300) \nРішення:  ${nocoins}`);
-
-let isOdd = nocoins % 2 !== 0;
-console.log(`Виведіть булеве значення: чи є сума всіх товарів (округлена в меншу сторону) парним чи непарним числом? \nРішення:  ${isOdd}`)
-
-let discount = Math.random() * (sneakers -  watch);
-discount = discount.toFixed(2);
-console.log(`Зробіть клієнту випадкову знижку та виведіть суму до оплати округлену до 2 знаків після коми. \nРішення:  ${discount}`);
+let phoneNumber = prompt('Введите номер телефона');
 
 
-minmax(sneakers, watch, notebook)   
+document.writeln(phoneNumber);
+
