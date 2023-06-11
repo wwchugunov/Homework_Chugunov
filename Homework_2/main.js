fb7408050230a67e9d069c90f51fe1b35320b539
wwchugunov <w.w.chugunov@gmail.com>
2023-06-12 01:05:26 +0300

    Cleaning index.js

--- Homework_2/main.js
+++ Homework_2/main.js
@@ -11,10 +11,6 @@ dntactvecs.addEventListener('click', function (e) {
     return;
   }
   
-
-// Тут якось зробив але всеж таки важко розуміти як воно працює в середині не не бачу явно ми складаемно число startnum 
-// Стільки раз скільки в змінній endnum я взагалі думав що треба зробити: додовати по одному поки не буде число дорывнювати endnum
-// Але судячи по завданню потрібно зовсім інше 
   for (result = 0; startnum <= endnum; startnum++) {
     if (clarification && startnum % 2 === 0) {
       continue;
