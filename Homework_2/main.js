c65c706eaa874c2029e46b7988513bbae9f9b3d1
wwchugunov <w.w.chugunov@gmail.com>
2023-06-12 00:10:35 +0300

    Create Homework_2 v1.0

--- Homework_2/main.js
+++ Homework_2/main.js
@@ -1,27 +1,19 @@
 const dntactvecs = document.getElementById('dntactvecs')
 
 
-
-
 dntactvecs.addEventListener('click', function (e) {
-    let startnum = prompt("Введите начальное число");
-    
-    if (!isNaN(parseInt(startnum))) {
-      // Код, который выполняется, когда startnum является числом
-      console.log("Введено целое число");
-    } else {
-      alert("Пожалуйста, введите целое число");
-      return; // Завершить выполнение функции, если введено неправильное значение
+    let startnum = parseInt(prompt("Введіть початкове число"));
+    let endnum = parseInt(prompt("Введіть кінцеве число"));  
+    let clarification = confirm("Пропускати парні числа")
+    if (isNaN(startnum) || isNaN(endnum)) {
+      alert("Введіть будь ласка ціле число");
+      return;
     }
-    
-    let clarification = confirm("Пропускать парные числа?");
-    
-    if (clarification === true) {
-      // Код, который выполняется, когда clarification равно true
-      console.log("Пропускаем парные числа");
-    } else {
-      // Код, который выполняется, когда clarification равно false
-      console.log("Не пропускаем парные числа");
+    for (; startnum <= endnum; startnum++) {
+      if (clarification && startnum % 2 === 0) {
+        continue;
+      }
+      console.log(startnum);
     }
   });
   
